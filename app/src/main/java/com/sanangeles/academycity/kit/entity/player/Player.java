e666279b4540d2197509b51043a6b34e684ad584
kokic <3250679033@qq.com>
2018-02-02 18:06:27 +0800

    #fix Player

--- app/src/main/java/com/sanangeles/academycity/kit/entity/player/Player.java
+++ app/src/main/java/com/sanangeles/academycity/kit/entity/player/Player.java
@@ -4,9 +4,5 @@ import com.sanangeles.academycity.*;
 
 public final class Player
 {
-	public static long player;
 	
-	public static void clearCarriedItem() {
-		Launcher.header.evaluate("Entity.setCarriedItem(getPlayerEnt(), 0, 0, 0);");
-	}
 }
