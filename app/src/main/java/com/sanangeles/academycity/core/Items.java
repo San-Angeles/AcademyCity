8ee652523a63e9f9f5e5025c88785531cd589dac
F_picacho <2314008827@qq.com>
2018-02-03 22:41:56 +0800

    定义铁矢

--- app/src/main/java/com/sanangeles/academycity/core/Items.java
+++ app/src/main/java/com/sanangeles/academycity/core/Items.java
@@ -7,7 +7,10 @@ public final class Items
 	public static ItemWrapper mCoin;
 	
 	public static final void initialization() {
-		mCoin = new ItemWrapper(new BaseItem(600, "coin", 1));
+		mCoin = new ItemWrapper(new BaseItem(600, "coin", 1)); //游戏币
 		ItemInstance.addItemCreative(600, 1, 0);
+		
+		mCoin = new ItemWrapper(new BaseItem(601, "tie", 1));  //铁矢
+		ItemInstance.addItemCreative(601, 1, 0);
 	}
 }
