cdf215c7e15dc9cdb5178163dc62a828a692afa4
int100 <1004121460@qq.com>
2018-02-04 08:20:41 +0800

    #20180204 fix

--- app/src/main/java/com/sanangeles/academycity/core/Items.java
+++ app/src/main/java/com/sanangeles/academycity/core/Items.java
@@ -5,12 +5,13 @@ import com.sanangeles.academycity.kit.item.*;
 public final class Items
 {
 	public static ItemWrapper mCoin;
+	public static ItemWrapper mTie;
 	
 	public static final void initialization() {
 		mCoin = new ItemWrapper(new BaseItem(600, "coin", 1)); //游戏币
 		ItemInstance.addItemCreative(600, 1, 0);
 		
-		mCoin = new ItemWrapper(new BaseItem(601, "tie", 1));  //铁矢
+		mTie = new ItemWrapper(new BaseItem(601, "tie", 1));  //铁矢
 		ItemInstance.addItemCreative(601, 1, 0);
 	}
 }
